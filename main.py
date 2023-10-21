5ed02e518c75a19c2252df4b8125eef229a78e7f
Sowmya <chattisowmya@gmail.com>
2023-10-21 11:32:59 -0400

    Added Anomaly Page

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from functions.get_data import get_query_data, execute_query
 from functions.get_query import read_query
 
 
+
 # page headings
 st.set_page_config(layout="wide", page_title="INFO7374: Algorithmic Marketing")
 st.title("INFO7374: Assignment 2")
-st.subheader("Group 1 (Team 2): Adit Bhosale, Sowmya Chatti, Vasundhara Sharma")
+st.subheader("Group 1 (Team 2): Adit Bhosle, Sowmya Chatti, Vasundhara Sharma")
 
 # ---------------------------------------------------------------
 st.header("Generate Data")
--- pages/anomaly.py
+++ pages/anomaly.py
@@ -1,3 +1,44 @@
+import datetime
 import streamlit as st
+import pandas as pd
+from functions.get_data import get_query_data, execute_query, execute_model
+from functions.get_query import read_query
+import altair as alt
+from main import date_param, days_param
 
-st.header("Anomaly Detection")
+
+st.subheader("Anamoly Detection")
+
+
+ls_date = date_param + datetime.timedelta(days=days_param+1)
+
+end_date = ls_date + datetime.timedelta(days=60)
+
+impressions = st.number_input("Choose the number of impressions to be checked", min_value=None, max_value=None, value=7000)
+date_param = st.date_input("Select Start Date:", value = ls_date,
+                                min_value=ls_date ,
+                                max_value= end_date)
+
+
+anomaly_model = read_query(f'queries/anomaly/anomaly_model.sql')
+anomaly_call = read_query(f'queries/anomaly/anomaly_call.sql').replace("{date_param}", date_param.strftime("%Y-%m-%d")).replace("{impression}", str(impressions))
+
+
+button_clicked = st.button('Execute', key=1002)
+if button_clicked:
+    
+    execute_model(anomaly_model)
+    
+    
+    df_1 = get_query_data(anomaly_call)
+    
+    ans = df_1['is_anomaly'].loc[0]
+    
+    
+    
+    if ans == True:
+        st.write("The impressions for the chosen date are an anomaly")
+        
+    else:
+         st.write("For the chosen date the number of impressions are not an anomaly")   
+st.markdown("---")
